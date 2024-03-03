63f966652e42cea61e8e018c488a6e12607fbcca
Nico Trummer <nictru32@gmail.com>
2024-03-03 09:21:00 +0000

    Implement MADs filter

--- src/sliders.py
+++ src/sliders.py
@@ -19,6 +19,7 @@ def slider_server(input, output, session,
                    _distributions: reactive.Value[Dict[str, Dict[str, float]]]
                    ):
     _adata_sample = reactive.value(None)
+    _prev_mads = reactive.value({})
 
     @output
     @render.text
@@ -33,7 +34,7 @@ def slider_server(input, output, session,
             return
 
         n_obs = adata.n_obs
-        return ui.input_slider('random_sample_size', 'Random sample size', min(100, n_obs), n_obs, min(10000, n_obs))
+        return ui.input_slider('random_sample_size', 'Random sample size', min(100, n_obs), n_obs, min(10000, n_obs), post=" cells")
     
     @reactive.effect
     def random_sample():
@@ -55,29 +56,66 @@ def slider_server(input, output, session,
         if distributions == {}:
             return ui.div(ui.p("No data available"))
 
-        sliders = []
+        panels = []
 
         for col, pretty_name in pretty_names.items():
             if distributions[col]['min'] == distributions[col]['max']:
                 continue
             else:
-                slider = ui.input_slider(col,
-                                pretty_name,
+                mads = ui.input_slider(f"{col}_mads", "MADs", 0.25, 10, 2, step=0.25)
+                absolute = ui.input_slider(f"{col}_absolute",
+                                "Absolute value",
                                 distributions[col]['min'],
                                 distributions[col]['max'],
                                 [distributions[col]['min'],
                                 distributions[col]['max']])
-                sliders.append(slider)
+                panel = ui.accordion_panel(pretty_name, mads, absolute)
+                panels.append(panel)
         
-        return ui.div(*sliders)
+        return ui.accordion(*panels)
+    
+    @reactive.effect
+    def update_absolute_by_mads():
+        pretty_names = _pretty_names.get()
+        distributions = _distributions.get()
+        prev_mads = _prev_mads.get()
+
+        if distributions == {}:
+            return
+
+        for col, _ in pretty_names.items():
+            if distributions[col]['min'] == distributions[col]['max']:
+                continue
+            else:
+                mads_name = f"{col}_mads"
+                absolute_name = f"{col}_absolute"
+
+                if not mads_name in input or not absolute_name in input:
+                    continue
+
+                mads = input[mads_name].get()
+                if mads_name in prev_mads and prev_mads[mads_name] == mads:
+                    continue
+
+                absolute = input[absolute_name].get()
+                if mads is None or absolute is None:
+                    continue
+
+                min_val = distributions[col]['median'] - mads * distributions[col]['std']
+                max_val = distributions[col]['median'] + mads * distributions[col]['std']
+
+                ui.update_slider(absolute_name, value=[min_val, max_val])
+
+                prev_mads[mads_name] = mads
     
     @reactive.effect
     def filter():
         def filter_row(row):
             for col in pretty_names.keys():
-                if not col in input:
+                input_name = f"{col}_absolute"
+                if not input_name in input:
                     continue
-                min_val, max_val = input[col].get()
+                min_val, max_val = input[input_name].get()
                 if not (min_val <= row[col] <= max_val):
                     return False
             return True
